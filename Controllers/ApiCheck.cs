a095ecb3dbe41d2e3a3255adc99e06cfd963ec08
DmitriyK97 <108519669+DmitriyK97@users.noreply.github.com>
2022-07-09 11:35:39 +0500

    Create ApiCheck.cs

---
+++ Controllers/ApiCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace TestA.Controllers
+{
+    [Table("histories")]
+    public class ApiCheck
+    {
+
+        [JsonPropertyName("id")]
+        [Column("id")]
+        [Key]
+        public int Id { get; set; }
+
+        [JsonPropertyName("check")]
+        [Column("check")]
+        public string Check { get; set; }
+
+        [JsonPropertyName("count")]
+        [Column("count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("date")]
+        [Column("date")]
+        public DateTime Date { get; set; }
+
+    }
+}
