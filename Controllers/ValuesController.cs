dfaf42660924d0ff8a74362c5baa7870561a116d
DmitriyK97 <108519669+DmitriyK97@users.noreply.github.com>
2022-07-09 11:35:42 +0500

    Create ValuesController.cs

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public void OnClick()
+        {
+            string text = "https://jsonplaceholder.typicode.com/posts";
+        }
+    }
+}
