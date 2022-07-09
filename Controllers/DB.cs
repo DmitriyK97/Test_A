10bdec48dda37b1d79a0ea58e3c1ed99a405367a
DmitriyK97 <108519669+DmitriyK97@users.noreply.github.com>
2022-07-09 11:35:36 +0500

    Create DB.cs

---
+++ Controllers/DB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace TestA.Controllers
+{
+    public class DB : DbContext
+    {
+        public DB()
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ApiCheck> Histories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+                $"Host=localhost;" +
+                $"Port=5433;" +
+                $"Database=test;" +
+                "TimeZone=Asia/Yekaterinburg;" +
+                $"Username=postgres;" +
+                $"Password=admin;" +
+                "Pooling=false;" +
+                "Timeout=600;" +
+                "CommandTimeout=600");
+        }
+    }
+}
